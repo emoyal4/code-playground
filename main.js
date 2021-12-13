05b294f9520b5482a3241406eb2170a957767667
emoyal4 <emoyal4@gmail.com>
2021-12-13 10:45:25 +0200

    add js editor

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict'
 let gCode = {}
 const gCodeOpen = {
-    css: true,
     html: true,
+    css: true,
+    js: true
 }
 let isNoChange = true
 
@@ -15,7 +16,8 @@ function initCode() {
     gCode = JSON.parse(localStorage.getItem('code')) || {}
     if (!gCode || !gCode.html || !gCode.css) {
         gCode.html = `<div class="box"></div>`
-        gCode.css = `.result{
+        gCode.js = `console.log('hello world')`
+        gCode.css = `.user-html{
     background: #fff;
     display: flex;
     flex-direction: column;
@@ -33,17 +35,25 @@ function initCode() {
 }
 
 function render() {
-    const elResult = document.querySelector('.result')
+    const elResult = document.querySelector('.user-html')
     elResult.innerHTML = gCode.html
 
-    const elStyle = document.querySelector('.user-style')
-    elStyle.innerText = gCode.css
+    const elUserCss = document.querySelector('.user-css')
+    elUserCss.innerText = gCode.css
+
+    const elUserJs = document.querySelector('.user-js')
+    console.log(elUserJs);
+    elUserJs.innerText = gCode.js
 
     const elHtml = document.querySelector('.html')
     elHtml.innerText = gCode.html
 
     const elCss = document.querySelector('.css')
     elCss.innerText = gCode.css
+
+    const elJs = document.querySelector('.js')
+    elJs.innerText = gCode.js
+
     document.querySelector('.save-btn').style.color = '#fff';
 }
 
@@ -70,18 +80,32 @@ function saveCode() {
     }
     gCode.html = _getHtmlFromString(document.querySelector('.html').innerText)
     gCode.css = _getHtmlFromString(document.querySelector('.css').innerText)
+    gCode.js = _getHtmlFromString(document.querySelector('.js').innerText)
     localStorage.setItem('code', JSON.stringify(gCode))
     isNoChange = true
     setUserMsg('Code Saved')
     render()
+    location.reload(true);
 }
 
 function resizeCode(ev) {
-    if (!ev.ctrlKey) return
-    const codeLng = ev.target.dataset.code === 'html' ? 'html' : 'css'
+    let codeLng = null
+    switch (ev.target.dataset.code) {
+        case 'html':
+            codeLng = 'html'
+            break;
+        case 'css':
+            codeLng = 'css'
+            break;
+        case 'js':
+            codeLng = 'js'
+            break;
+        default:
+            break;
+    }
     gCodeOpen[codeLng] = !gCodeOpen[codeLng]
-    const elCode = document.querySelector(`.${codeLng}`)
-    if (gCodeOpen[codeLng]) {
+    const elCode = document.querySelector(`.${codeLng}-container`)
+    if (!gCodeOpen[codeLng]) {
         elCode.classList.remove('open')
     } else {
         elCode.classList.add('open')
@@ -109,11 +133,24 @@ function setUserMsg(msg) {
 }
 
 function downloadCode() {
-    for (var i = 0; i < 2; i++) {
-        const codeLng = (!i) ? 'html' : 'css'
-        const fileName = (!i) ? 'index.html' : 'style.css'
+    const lngs = Object.keys(gCode)
+    let fileName
+    for (var i = 0; i < lngs.length; i++) {
+        switch (lngs[i]) {
+            case 'html':
+                fileName = 'index.html'
+                break;
+            case 'css':
+                fileName = 'style.css'
+                break;
+            case 'js':
+                fileName = 'main.js'
+                break;
+            default:
+                break;
+        }
         var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(gCode[codeLng]));
+        element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(gCode[lngs[i]]));
         element.setAttribute('download', fileName);
         element.style.display = 'none';
         document.body.appendChild(element);
